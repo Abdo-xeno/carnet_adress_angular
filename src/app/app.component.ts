1d5e9a1d88442d70c9037ea228d5791627c2f9cd
Abdo-xeno <naananiabdelkodouss@gmail.com>
2021-08-28 22:47:47 +0100

    maj app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mon-projet-openclass';
+export class AppComponent implements OnInit {
+  isAuth = false;
+
+  appareils: any[] | undefined;
+  
+  
+
+ 
+
+  constructor(private appareilService:AppareilService){
+    setTimeout(
+      ()=>{
+        this.isAuth = true;
+      },4000
+    )
+
+    }
+    
+    onAllumer(){
+      this.appareilService.switchOnAll();
+    }
+
+    onEteindre(){
+      {
+      this.appareilService.switchOffAll();
+    } 
+    }
+
+  ngOnInit(){
+    this.appareils = this.appareilService.appareils;
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,71 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppareilService } from './services/appareil.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppareilComponent } from './appareil/appareil.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { AgGridModule } from 'ag-grid-angular';
+import { UsersGridComponent } from './users-grid/users-grid.component';
+
+import { DataService } from './data.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+import { BtnCellRenderer } from './button-update/button-update.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ButtonDeleteComponent } from './button-delete/button-delete.component';
+
+const appRoutes: Routes = [
+  { path: 'contactForm', component: ContactFormComponent },
+  { path: 'list', component: UsersGridComponent },
+  { path: 'contactForm/:id', component: ContactFormComponent  },
+  
+];
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    AppareilComponent,
+    ContactFormComponent,
+    UsersGridComponent,
+    BtnCellRenderer,
+    ButtonDeleteComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    AgGridModule.withComponents([BtnCellRenderer]),
+    environment.production ?[]: HttpClientInMemoryWebApiModule.forRoot(DataService),
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+    
   ],
-  providers: [],
+  exports:[ContactFormComponent],
+  providers: [
+    AppareilService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
